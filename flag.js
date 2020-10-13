3da22e3f0113561fc95ad25d2e1ee01d7832d2fc
Lujain AbdUllatif <lujain.abdallatif@mail.huji.ac.il>
2020-10-13 15:41:04 +0300

    Adding the flags' object

---
+++ flag.js
@@ -0,0 +1,23 @@
+// the flags' object >> key = countryname, flage = src
+
+const flagsObj = {
+    'CANADA': '/img/CANADA.jpg',
+    'CHILE': '/img/CHILE.jpg',
+    'CHINA': '/img/CHINA.jpg',
+    'COLOMBIA': '/img/COLOMBIA.jpg',
+    'EGYPT': '/img/EGYPT.jpg',
+    'IRAN': '/img/IRAN.jpg',
+    'IRAQ': '/img/ISRAEL.jpg',
+    'ITALY': '/img/ITALY.jpg',
+    'JAPAN': '/img/JAPAN.jpg',
+    'JORDAN': '/img/JORDAN.jpg',
+    'LEBANON': '/img/LEBANON.jpg',
+    'SAUDIARABIA': '/img/SAUDIARABIA.jpg',
+    'SPAIN': '/img/SPAIN.jpg',
+    'SUDAN': '/img/SUDAN.jpg',
+    'TURKEY': '/img/TURKEY.jpg',
+    'UNITEDSTATES': '/img/UNITEDSTATES.jpg'
+}
+
+
+
