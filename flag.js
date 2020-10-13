59282a590044600d3d8c9e69eb785f8b579c7d6d
mj majadly <majadly@nevada.unr.edu>
2020-10-13 17:03:43 +0300

    main3

--- flag.js
+++ flag.js
@@ -1,5 +1,14 @@
 // the flags' object >> key = countryname, flage = src
+function createNewElement() {
+    // First create a DIV element.
+	var txtNewInputBox = document.createElement('div');
 
+    // Then add the content (a new input box) of the element.
+	txtNewInputBox.innerHTML = "<input type='text' id='newInputBox'>";
+
+    // Finally put it where it is supposed to appear.
+	document.getElementById("newElementId").appendChild(txtNewInputBox);
+}
 const flagsObj = {
     'CANADA': '/img/CANADA.jpg',
     'CHILE': '/img/CHILE.jpg',
