4e3737e3a6bc8967653e710bad3f5dbc9018f4c5
nuwrss <nuwrssazzam@gmail.com>
2020-10-14 16:58:58 +0300

    adde sounds

--- flag.js
+++ flag.js
@@ -17,10 +17,31 @@ const flagsObj = {
     // 'SUDAN': 'img/SUDAN.jpg',
     // 'TURKEY': 'img/TURKEY.jpg',
     // 'UNITEDSTATES': 'img/UNITEDSTATES.jpg'
+};
+
+var correctSound = new sound("sounds/correctsound.mp3");
+var wrongSound= new sound("sounds/wrongsound.mp3");
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }    
 }
 
+
+
+
 var imgflag = document.getElementById("imgflag");
-var input = document.getElementById("input");
+var input = document.getElementById("inputAnswer");
 input.style.display = 'none';
 var submit = document.getElementById("submit");
 let myObjKeys = Object.keys(flagsObj);
@@ -43,6 +64,7 @@ var countryName = '';
 var wrongAnswer = -5 ;
 var correctAnswer = 10 ;
 var userName = '';
+var numberCorrectAnswers = 0 ;
 
 
 
@@ -51,7 +73,8 @@ function setCountry() {
     if (level == myObjKeys.length) {
 
 
-        var queryString = "?score=" + score + "&userName=" + userName;
+        var queryString = "?score=" + score + "&userName=" + userName +"&numberCorrectAnswers="+numberCorrectAnswers;
+        console.log(queryString);
         window.location.href = "endgame.html" + queryString;
 
     } else {
@@ -74,17 +97,21 @@ function submitMe() {
         input.style.display = 'block';
         scoreText.style.display = 'block';
         skipperButton.style.display= 'block';
+        userName=userNameInout.value;
         userNameInout.style.display = 'none';
-        userName=userNameInout.textContent;
+        
         setCountry();
 
     } else {
         if (input.value.toLowerCase() === countryName.toLowerCase()) {
             level++;
             score+=correctAnswer;
+            numberCorrectAnswers++;
+            correctSound.play();
             setCountry();
 
         } else {
+            wrongSound.play();
             score+=wrongAnswer;
             alert('Wrong !! Try Again ');
 
