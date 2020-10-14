69522a962423d420d629e76c4c47346246e6af66
nuwrss <nuwrssazzam@gmail.com>
2020-10-14 22:48:35 +0300

    fixed small screen issue

--- src/js/endgame.js
+++ src/js/endgame.js
@@ -25,7 +25,7 @@ function setting(msg, gf) {
 
     score.textContent = obj.score;
     username.textContent = obj.userName;
-    correctanswers.textContent = obj.numberCorrectAnswers;
+    correctanswers.textContent = obj.numberCorrectAnswers + ' Out Of 20';
 
     message.textContent = msg;
     gif.setAttribute('src',"../../img/"+ gf)
